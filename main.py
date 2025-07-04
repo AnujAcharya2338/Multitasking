dfe85ceff6d69b03546e06e2b1e2bb64161486b0
AnujAcharya2338 <anujacharya877@gmail.com>
2025-07-04 05:28:10 -0700

    Filw handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-file = open("my_file.txt")
-
-contents = file.read()
-print(contents)
-file.close()
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents) 
+    
+with open("new_file1.txt", mode="w") as file:
+    file.write(" \n Congrats for being sucessful" )
